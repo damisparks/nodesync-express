6a7c6e624d089fbe799fd8e7ef0dd7097dd09f05
Damilola Omifare <omifaredammy@gmail.com>
2023-06-21 13:38:53 +0100

    feat: add data modelling

---
+++ src/app/models/countryModel.ts
@@ -0,0 +1,29 @@
+import {model, Schema} from 'mongoose';
+import {ICountry} from '@/interfaces/countryInterface';
+
+const countrySchema = new Schema<ICountry>({
+  name: {
+    type: String,
+    required: true,
+  },
+  common: {type: String, default: ''},
+  independent: {type: Boolean, default: false},
+  status: {type: String, default: ''},
+  subregion: {type: String, default: ''},
+  maps: {
+    googleMaps: {type: String, default: ''},
+    openStreetMaps: {type: String, default: ''},
+  },
+  area: {type: Number, default: 0},
+  timezones: {type: [String], default: []},
+  flags: {
+    svg: {type: String, default: ''},
+    png: {type: String, default: ''},
+  },
+  capitalInfo: {
+    latlng: {type: [Number], default: []},
+  },
+  startOfWeek: {type: String, default: ''},
+});
+
+export const Campus = model<ICountry>('Country', countrySchema, 'countries');

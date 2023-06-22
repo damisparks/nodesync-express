be040a163723e68c5e8c620f1b8f5bee44d93963
Damilola Omifare <omifaredammy@gmail.com>
2023-06-22 11:22:48 +0100

    fix: add logs to checkauth middleware

--- src/api/config/middlewares/checkAuth.ts
+++ src/api/config/middlewares/checkAuth.ts
@@ -3,7 +3,6 @@ import {logger} from '@/infrastructure/logging/logger';
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
   // Check for basic auth header
-  logger.info('basicAuth', req.headers);
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const encodedCredentials = authHeader.split(' ')[1];
@@ -11,6 +10,7 @@ export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
       'utf-8'
     );
     const [username, password] = credentials.split(':');
+    logger.info(`username: ${username}, password: ${password}`);
 
     // Check username and password
     if (username === 'admin' && password === 'password') {
@@ -20,6 +20,7 @@ export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
   }
 
   // Unauthorized
+  logger.info('Unauthorized');
   res.setHeader('WWW-Authenticate', 'Basic realm="Restricted"');
   res.status(401).send('Unauthorized');
 };
